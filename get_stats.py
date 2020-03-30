4bb16a13150efcce05c5fdbb188283d56ee4bdd9
Ryan Keller <ryan.keller@hanzoarchives.com>
2020-03-30 18:23:41 +0000

    added examples

--- get_stats.py
+++ get_stats.py
@@ -5,6 +5,10 @@ def main():
     name = sys.argv[1]
     s = Scoreboard("games.csv")
     print (s.get_win_pct_leaderboard(min_games=3))
+    print ("---------------")
+    print (s.get_wins_leaderboard())
+    print ("---------------")
+    print (s.get_player_stats("Neville"))
 
 if __name__ == "__main__":
     main()
