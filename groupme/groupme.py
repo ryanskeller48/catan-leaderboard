5a07d62e1f1850e0bf4a1b8940f5f3a338fa73ee
ryanskeller48 <keller@DESKTOP-QVH7E87.localdomain>
2020-04-08 10:52:28 -0400

    finished filtering, added more helper functions, finished pagination

--- groupme/groupme.py
+++ groupme/groupme.py
@@ -57,16 +57,34 @@ class GroupMe:
 
     def _epoch_to_datetime(self, epoch):
         """ make datetime object out of seconds from epoch time """
-        # TODO: implement
-        return
+
+        return datetime.fromtimestamp(int(epoch))
 
     def get_groups(self):
         """ get all groups the signed-in user is subscribed to """
+
+        all_groups = []
+
+        params = {"token": self.api_token, "per_page": 10}
+        tick = 1
+        params['page'] = tick
   
-        params = {"token": self.api_token}
         response = self._api_request("groups", params=params)
-        if response is not None and response != []:
-            return response['response']
+
+        while response['response'] is not None and response['response'] != []:
+            all_groups += response['response']
+            tick += 1
+            params['page'] = tick
+            response = self._api_request("groups", params=params)
+
+        return all_groups
+
+    def get_group_id(self, name):
+
+        groups = self.get_groups()
+        for group in groups:
+            if group['name'] == name:
+                return group['id']
 
     def get_group_members(self, name=None, groupid=None):
         """ get list of members for a group """
@@ -85,8 +103,12 @@ class GroupMe:
                 if member['name'] == name or member['nickname'] == nickname:
                     return member['user_id']
 
-    def get_1page_messages(self, groupid, before=0, group=False, chat=False, chatid=None):
+    def get_1page_messages(self, groupid=None, groupname=None, before=0, group=False, chat=False, chatid=None):
         """ by default, messages return 20 at a time, so need to paginate to get all """
+
+        if not groupname and not groupid: return None
+        elif groupname and not groupid:
+            groupid = self.get_group_id(groupname)
 
         params = {"token": self.api_token, "limit": 20}
         
@@ -108,43 +130,56 @@ class GroupMe:
             else:
                 return response['response']['messages']
 
-    def get_all_messages(self, groupid, group=False, chat=False, chatid=None):
+    def get_all_messages(self, groupid=None, groupname=None, group=False, chat=False, chatid=None):
         """ helper to paginate messages -- paginates on id of last message """
 
         params = {"token": self.api_token, "limit": 20}
         all_messages = []
 
-        some_messages = self.get_1page_messages(groupid, group=group, chat=chat, chatid=chatid)
+        some_messages = self.get_1page_messages(groupid=groupid, groupname=groupname, group=group, chat=chat, chatid=chatid)
 
         while some_messages is not None and len(some_messages) > 0:
             last_id = some_messages[-1]['id']
             all_messages += some_messages
-            some_messages = self.get_1page_messages(groupid, before=last_id, group=group, chat=chat, chatid=chatid)
+            some_messages = self.get_1page_messages(groupid=groupid, groupname=groupname, before=last_id, group=group, chat=chat, chatid=chatid)
 
         return all_messages
 
     def get_chats(self):
         """ get all direct messages for signed-in user """
 
-        params = {"token": self.api_token}
+        all_chats = []
+
+        params = {"token": self.api_token, "per_page": 10}
+        tick = 1
+        params['page'] = tick
+
         response = self._api_request("chats", params=params)
-        if response is not None and response != []:
-            return response['response']
+
+        while response['response'] is not None and response['response'] != []:
+            all_chats += response['response']
+            tick += 1
+            params['page'] = tick
+            response = self._api_request("chats", params=params)
+
+        return all_chats
 
     def filter_messages(self, messages, user=None, userid=None, text=None, dateOn=None, dateBefore=None, dateAfter=None):
         """ filter messages by text, sender, date, etc. """
 
         filtered = []
 
-        if userid is None and user is not None: # TODO: finish implementation and test
-            groupid = "" # TODO: grab group id (or group name) from messages
+        if userid is None and user is not None:
+            groupid = messages[0]['group_id']
             group_members = self.get_group_members(groupid=groupid)
-            userid = self.get_user_id(group_members, name=user)
+            userid = self.get_user_id(group_members, name=user, nickname=user)
 
         for message in messages:
             passes = True
+            raw_time = message['created_at']
+            timestamp = self._epoch_to_datetime(raw_time).date()
 
-            if user: # TODO: finish implementation and test
+            if user:
                 if 'sender_id' in message:
                     if message['sender_id'] != userid:
                         passes = False
@@ -161,7 +196,23 @@ class GroupMe:
                 else:
                     passes = False
 
-            # TODO: implement time-based filtering
+            if dateOn:
+                if timestamp != dateOn:
+                    passes = False
+
+            elif dateBefore and dateAfter:
+                if (dateBefore - timestamp) < timedelta() or (timestamp - dateAfter) < timedelta():
+                    passes = False
+
+            elif dateBefore:
+                # either before OR on this date
+                if (dateBefore - timestamp) < timedelta():
+                    passes = False
+
+            elif dateAfter:
+                # either on OR after this date
+                if (timestamp - dateAfter) < timedelta():
+                    passes = False
 
             if passes:
                 filtered += [message]
